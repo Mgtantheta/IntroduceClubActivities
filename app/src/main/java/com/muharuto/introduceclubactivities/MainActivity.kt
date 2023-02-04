68b3761797d14d1007b758c2ade80c23665c4840
Mgtantheta <freecityNJ@gmail.com>
2023-02-04 19:12:32 +0900

    Bind fragment

--- app/src/main/java/com/muharuto/introduceclubactivities/MainActivity.kt
+++ app/src/main/java/com/muharuto/introduceclubactivities/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragmentTransaction = supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container_view, HomeFragment())
+            .addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
