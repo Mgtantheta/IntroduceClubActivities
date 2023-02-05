ce7df7ad3fd7c03a4e02425bf56a8f6170a6d294
Mgtantheta <freecityNJ@gmail.com>
2023-02-05 10:39:38 +0900

    delete import android.content.Content

--- app/src/main/java/com/muharuto/introduceclubactivities/ClubAdapter.kt
+++ app/src/main/java/com/muharuto/introduceclubactivities/ClubAdapter.kt
@@ -1,6 +1,5 @@
 package com.muharuto.introduceclubactivities
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
