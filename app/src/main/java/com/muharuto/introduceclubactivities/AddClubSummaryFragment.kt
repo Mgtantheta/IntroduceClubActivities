864dc194c27088dbdf75d510d11f2dbb190e53a3
Mgtantheta <freecityNJ@gmail.com>
2023-02-06 18:18:24 +0900

    add AddClubSummaryFragment

---
+++ app/src/main/java/com/muharuto/introduceclubactivities/AddClubSummaryFragment.kt
@@ -0,0 +1,21 @@
+package com.muharuto.introduceclubactivities
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.muharuto.introduceclubactivities.databinding.FragementAddClubSummaryBinding
+
+class AddClubSummaryFragment : Fragment(R.layout.fragement_add_club_summary) {
+    private var fragmentAddClubSummaryBinding: FragementAddClubSummaryBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragementAddClubSummaryBinding.bind(view)
+        fragmentAddClubSummaryBinding = binding
+    }
+
+    override fun onDestroyView() {
+        fragmentAddClubSummaryBinding = null
+        super.onDestroyView()
+    }
+}
