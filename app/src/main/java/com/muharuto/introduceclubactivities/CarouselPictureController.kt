bf5208d769ed1d6122e8672f5930a501e79dc06b
Mgtantheta <freecityNJ@gmail.com>
2023-02-13 15:14:08 +0900

    wip epoxy carousel

--- app/src/main/java/com/muharuto/introduceclubactivities/CarouselPictureController.kt
+++ app/src/main/java/com/muharuto/introduceclubactivities/CarouselPictureController.kt
@@ -4,28 +4,19 @@ import com.airbnb.epoxy.Carousel
 import com.airbnb.epoxy.TypedEpoxyController
 import com.airbnb.epoxy.carousel
 import com.muharuto.introduceclubactivities.data.ClubSummary
+import withModelsFrom
 
 class CarouselPictureController internal constructor() : TypedEpoxyController<List<ClubSummary>>() {
     override fun buildModels(data: List<ClubSummary>?) {
         data ?: return
 
-        val epoxyLayoutCarouselImages: MutableList<EpoxyLayoutCarouselImageBindingModel_> =
-            mutableListOf()
-        data.forEach { clubSummary ->
-            epoxyLayoutCarouselImages.add(
-                EpoxyLayoutCarouselImageBindingModel_()
-                    .id(clubSummary.id)
-                    .clubSummaryModel(clubSummary)
-                //Fixme: 後で消す
-//                    .addTo(this@CarouselPictureController)
-            )
-        }
-
         carousel {
             id("carousel")
             numViewsToShowOnScreen(1.05f)
             padding(Carousel.Padding.dp(12, 0))
-            models(epoxyLayoutCarouselImages)
+            withModelsFrom(data) {
+                EpoxyLayoutCarouselImageBindingModel_().id(it.id).clubSummaryModel(it)
+            }
         }
     }
 }
