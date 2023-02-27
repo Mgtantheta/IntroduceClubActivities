9c642873ff27304039eda8a589ae1dc3d9b22cf9
Mgtantheta <freecityNJ@gmail.com>
2023-02-27 16:27:35 +0900

    add insert method

--- app/src/main/java/com/muharuto/introduceclubactivities/database/clubsummarydata/ClubSummaryDao.kt
+++ app/src/main/java/com/muharuto/introduceclubactivities/database/clubsummarydata/ClubSummaryDao.kt
@@ -1,10 +1,16 @@
 package com.muharuto.introduceclubactivities.database.clubsummarydata
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 
 @Dao
 interface ClubSummaryDao {
     @Query("SELECT * FROM clubSummaryDB")
     fun getAll(): List<ClubSummaryData>
+
+    @Insert
+    fun insert(clubSummaryData: ClubSummaryData)
+
+    // TODO: UpdateとDeleteを後で作成
 }
