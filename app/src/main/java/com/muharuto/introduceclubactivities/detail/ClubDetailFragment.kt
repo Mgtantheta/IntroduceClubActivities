728d7e84906e39bd4730b2b690c22f35dfe01d0c
Mgtantheta <freecityNJ@gmail.com>
2023-03-14 15:47:35 +0900

    change the order

--- app/src/main/java/com/muharuto/introduceclubactivities/detail/ClubDetailFragment.kt
+++ app/src/main/java/com/muharuto/introduceclubactivities/detail/ClubDetailFragment.kt
@@ -3,38 +3,23 @@ package com.muharuto.introduceclubactivities.detail
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.muharuto.introduceclubactivities.ClubSummaryApplication
 import com.muharuto.introduceclubactivities.R
-import com.muharuto.introduceclubactivities.database.clubsummarydata.ClubSummaryData
 import com.muharuto.introduceclubactivities.databinding.FragmentClubDetailBinding
 
 class ClubDetailFragment : Fragment(R.layout.fragment_club_detail) {
 
     private var _binding: FragmentClubDetailBinding? = null
-    private val binding get() = _binding!!
 
-    lateinit var clubSummaryData: ClubSummaryData
-
-    private val viewModel: ClubViewModel by activityViewModels {
-        ClubViewModelFactory(
-            (activity?.application as ClubSummaryApplication).database.clubSummaryDao()
-        )
-    }
-
-    private fun bind(clubSummaryData: ClubSummaryData) {
-        binding.apply {
-            binding.clubName.text = clubSummaryData.clubName
-            binding.clubSentence.text = clubSummaryData.clubSentence
-            binding.clubActivityDay.text = clubSummaryData.clubActivityDay
-            binding.activityPlace.text = clubSummaryData.activityPlace
-            binding.clubRepresentatives.text = clubSummaryData.clubRepresentative
-            binding.clubRepresentativeId.text = clubSummaryData.clubRepresentativeId
-        }
-    }
-
+    //TODO: 後で使う
+//    private val binding get() = _binding!!
+//    lateinit var clubSummaryData: ClubSummaryData
+//    private val viewModel: ClubViewModel by activityViewModels {
+//        ClubViewModelFactory(
+//            (activity?.application as ClubSummaryApplication).database.clubSummaryDao()
+//        )
+//    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -55,4 +40,16 @@ class ClubDetailFragment : Fragment(R.layout.fragment_club_detail) {
         super.onDestroyView()
         _binding = null
     }
+
+//TODO: 後で使う
+//    private fun bind(clubSummaryData: ClubSummaryData) {
+//        binding.apply {
+//            binding.clubName.text = clubSummaryData.clubName
+//            binding.clubSentence.text = clubSummaryData.clubSentence
+//            binding.clubActivityDay.text = clubSummaryData.clubActivityDay
+//            binding.activityPlace.text = clubSummaryData.activityPlace
+//            binding.clubRepresentatives.text = clubSummaryData.clubRepresentative
+//            binding.clubRepresentativeId.text = clubSummaryData.clubRepresentativeId
+//        }
+//    }
 }
