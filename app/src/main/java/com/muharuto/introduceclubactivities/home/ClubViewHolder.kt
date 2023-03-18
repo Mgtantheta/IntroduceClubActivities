73329967e6e0f62b28d1e470c957de5d2c3555d2
Mgtantheta <freecityNJ@gmail.com>
2023-03-18 11:55:21 +0900

    able to bind the home screen.

--- app/src/main/java/com/muharuto/introduceclubactivities/home/ClubViewHolder.kt
+++ app/src/main/java/com/muharuto/introduceclubactivities/home/ClubViewHolder.kt
@@ -3,13 +3,23 @@ package com.muharuto.introduceclubactivities.home
 import android.content.Context
 import android.graphics.BitmapFactory
 import androidx.recyclerview.widget.RecyclerView
-import com.muharuto.introduceclubactivities.R
 import com.muharuto.introduceclubactivities.database.clubsummarydata.ClubSummaryData
 import com.muharuto.introduceclubactivities.databinding.ClubSummaryItemBinding
 
-class ClubViewHolder(private val binding: ClubSummaryItemBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(clubSummaryData: ClubSummaryData, context: Context, onItemClickListener: ClubAdapter.OnItemClickListener) {
-        binding.clubImage.setImageBitmap(BitmapFactory.decodeResource(context.resources, R.drawable.sample))
+class ClubViewHolder(private val binding: ClubSummaryItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(
+        clubSummaryData: ClubSummaryData,
+        context: Context,
+        onItemClickListener: ClubAdapter.OnItemClickListener
+    ) {
+        binding.clubImage.setImageBitmap(
+            BitmapFactory.decodeByteArray(
+                clubSummaryData.clubImage,
+                0,
+                clubSummaryData.clubImage.size
+            )
+        )
         binding.clubName.text = clubSummaryData.clubName
         binding.activityDayOfWeek.text = clubSummaryData.clubActivityDay
         binding.root.setOnClickListener {
