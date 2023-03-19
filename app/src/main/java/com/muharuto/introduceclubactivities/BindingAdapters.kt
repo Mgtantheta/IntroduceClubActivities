906d5e28e86bd7005289ad85fff78ae6d0629f03
Mgtantheta <freecityNJ@gmail.com>
2023-03-19 18:10:19 +0900

    do refactor

--- app/src/main/java/com/muharuto/introduceclubactivities/BindingAdapters.kt
+++ app/src/main/java/com/muharuto/introduceclubactivities/BindingAdapters.kt
@@ -1,16 +1,12 @@
 package com.muharuto.introduceclubactivities
 
-import android.graphics.Bitmap
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.muharuto.introduceclubactivities.data.HomeClubSummary
 
 @BindingAdapter("bitmapSrc")
 fun ImageView.setBitmapSrc(homeClubSummary: HomeClubSummary?) {
-    if (homeClubSummary != null) {
-        val bitmap = homeClubSummary.getImageBitmap()
-        setImageBitmap(bitmap)
-    } else {
-        setImageResource(android.R.color.transparent)
-    }
+    homeClubSummary?.let {
+        setImageBitmap(it.getImageBitmap())
+    } ?: setImageResource(android.R.color.transparent)
 }
